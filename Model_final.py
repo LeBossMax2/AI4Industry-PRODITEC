50819d527e005618fea576c24538a91a91424b20
Lucas Guedon <guedon.lucas@laposte.net>
2022-01-17 16:25:43 +0100

    Reduce learning rate

--- Model_final.py
+++ Model_final.py
@@ -7,86 +7,79 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
 
 import pathlib
 import shutil
 
-class Ai :
-  
-  def train():
+batch_size = 64
+learning_rate = 0.0002
+epochs = 20
 
-    global train_ds
-    global val_ds
-    global class_names
+data_dir = "data/IA" # modifier data pour n'avoir que deux categorie/fichier
+data_dir = pathlib.Path(data_dir)
 
-    data_dir = "data/IA" # modifier data pour n'avoir que deux categorie/fichier
-    data_dir = pathlib.Path(data_dir)
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(480, 640),
+  batch_size=batch_size)
 
-    train_ds = tf.keras.utils.image_dataset_from_directory(
-      data_dir,
-      validation_split=0.2,
-      subset="training",
-      seed=123,
-      image_size=(480, 640),
-      batch_size=32)
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(480, 640),
+  batch_size=batch_size)
 
-    val_ds = tf.keras.utils.image_dataset_from_directory(
-      data_dir,
-      validation_split=0.2,
-      subset="validation",
-      seed=123,
-      image_size=(480, 640),
-      batch_size=32)
+class_names = train_ds.class_names
 
-    class_names = train_ds.class_names
+AUTOTUNE = tf.data.AUTOTUNE
 
-    AUTOTUNE = tf.data.AUTOTUNE
+#train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+#val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+num_classes = len(class_names)
 
-    num_classes = len(class_names)
+def train():
 
     model = Sequential([
-      layers.Rescaling(255, input_shape=(480, 640, 3)),
-      layers.Conv2D(16, 3, padding='same', activation='relu'),
-      layers.MaxPooling2D(),
-      layers.Conv2D(32, 3, padding='same', activation='relu'),
-      layers.MaxPooling2D(),
-      layers.Conv2D(64, 3, padding='same', activation='relu'),
-      layers.MaxPooling2D(),
-      layers.Flatten(),
-      layers.Dense(128, activation='relu'),
-      layers.Dense(num_classes)
+        layers.Rescaling(255, input_shape=(480, 640, 3)),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes)
     ])
 
-    model.compile(optimizer='adam',
+    model.compile(optimizer=Adam(learning_rate),
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
 
-    epochs = 6
     history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs,
+        callbacks=[EarlyStopping(patience=4)]
     )
 
     print('\nSaving...')
-    model.save("AI4Indistry-PRODITEC/My_model")
+    model.save("My_model")
     print('Ok')
 
-  def predict(path) :
+def predict(data_test) :
 
-    model = keras.models.load_model("AI4Indistry-PRODITEC/My_model")
+    model = keras.models.load_model("My_model")
 
-    data_test = pathlib.Path(path)
-    
-    img = tf.keras.utils.load_img(
-        data_test 
-    )
-
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)
+    img_array = tf.expand_dims(data_test, 0)
 
     predictions = model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
@@ -96,7 +89,7 @@ class Ai :
         .format(class_names[np.argmax(score)], 100 * np.max(score))
     )
 
-#Ai.train()
+#train()
 
-Ai.predict("AI4Indistry-PRODITEC/data/IA/CAT2JPG/CAM1-2020-12-09 11-41-41-749.JPG")
+predict(next(iter(val_ds))[0][0])
   
