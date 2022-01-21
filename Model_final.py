f839a99b76c9f391ddfc065bd9c0fb866b41f73b
Lucas Guedon <guedon.lucas@laposte.net>
2022-01-21 08:45:32 +0100

    Add class weights

--- Model_final.py
+++ Model_final.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import PIL
 import tensorflow as tf
+from sklearn.utils import class_weight
 
 from tensorflow import keras
 import tensorflow.keras.backend as K
@@ -15,9 +16,9 @@ from tensorflow.keras.metrics import Recall
 import pathlib
 import shutil
 
-batch_size = 128 # TODO tester un batch size plus gros
-learning_rate = 0.0002
-epochs = 30
+batch_size = 128
+learning_rate = 0.00002
+epochs = 800
 
 data_dir = "data/IA_test" # modifier data pour n'avoir que deux categorie/fichier
 data_dir = pathlib.Path(data_dir)
@@ -37,7 +38,7 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
   validation_split=0.2,
   subset="training",
   seed=123,
-  image_size=(480, 480),
+  image_size=(258, 258),
   crop_to_aspect_ratio=True,
   batch_size=batch_size)
 
@@ -49,7 +50,7 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   validation_split=0.2,
   subset="validation",
   seed=123,
-  image_size=(480, 480),
+  image_size=(258, 258),
   crop_to_aspect_ratio=True,
   batch_size=batch_size).map(preprocess_image)
 
@@ -72,7 +73,7 @@ def positive_accuracy(true_label, pred):
     true_pred = K.round(tf.boolean_mask(pred, tf.math.less(true_label, tf.constant([3.0])), axis = 0))
     return tf.cond(
         tf.size(true_pred) == 0,
-        lambda: 0.,
+        lambda: tf.constant([1.]),
         lambda: K.mean(tf.math.less(true_pred, tf.constant([3.0])))
     )
 
@@ -80,13 +81,13 @@ def negative_accuracy(true_label, pred):
     false_pred = K.round(tf.boolean_mask(pred, tf.math.greater_equal(true_label, tf.constant([3.0])), axis = 0))
     return tf.cond(
         tf.size(false_pred) == 0,
-        lambda: 0.,
+        lambda: tf.constant([1.]),
         lambda: K.mean(tf.math.greater_equal(false_pred, tf.constant([3.0])))
     )
 
 def build_model():
     model = keras.applications.ResNet50V2(include_top=True, weights=None, classes=1, classifier_activation=None)
-#
+
     model.compile(optimizer=Adam(learning_rate),
                   loss="mse",
                   metrics=['accuracy', positive_accuracy, negative_accuracy])
@@ -96,12 +97,31 @@ def train():
 
     model = build_model()
 
+    y_train = np.concatenate([y.numpy() for x, y in val_ds])
+    print(y_train)
+    class_weights = class_weight.compute_class_weight(class_weight = 'balanced', classes = np.arange(0, num_classes), y = y_train)
+    class_weights = dict(zip(np.arange(0, num_classes), class_weights))
+
+    print(class_weights)
+
     history = model.fit(
         train_ds,
         validation_data=val_ds,
+        class_weight=class_weights,
         epochs=epochs,
-        callbacks=[EarlyStopping(patience=8, restore_best_weights=True)]
+        callbacks=[EarlyStopping(patience=20, restore_best_weights=True)]
     )
+
+    plt.figure(figsize=(15,5))
+    for i, t in enumerate(["loss", "accuracy", "positive_accuracy", "negative_accuracy"]):
+        plt.subplot(1, 4, i + 1)
+        plt.plot(history.history[t], label=t+" train")
+        plt.plot(history.history["val_" + t], label=t+" val")
+        plt.title('model ' + t)
+        plt.ylabel(t)
+        plt.xlabel('epoch')
+        plt.legend(loc='upper left')
+    plt.savefig("history.png")
 
     print('\nSaving...')
     model.save_weights("My_model")
@@ -124,7 +144,7 @@ def evaluate():
     
     tf.print(K.sum(tf.linalg.diag_part(mat)) / K.sum(mat), K.sum(mat[:3, :3]) / K.sum(mat[:3, :]), K.sum(mat[3:, 3:]) / K.sum(mat[3:, :]))
 
-def predict(data_test) :
+def predict(data_test, cat) :
 
     model = build_model()
     model.load_weights("My_model")
@@ -134,10 +154,10 @@ def predict(data_test) :
     predictions = model.predict(img_array)
     score = predictions[0]
 
-    print("The score of this image is", score)
+    print("The score of this image is", score, ", actuel category", cat)
 
 train()
 
 evaluate()
 
-predict(next(iter(val_ds))[0][0])
+predict(next(iter(val_ds))[0][0], next(iter(val_ds))[1][0])
