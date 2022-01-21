307e43d62bf3725df3d5d10cf9e23bd292ba5b45
Lucas Guedon <guedon.lucas@laposte.net>
2022-01-21 11:51:17 +0100

    Add comments to pre-processing

--- Preprod.py
+++ Preprod.py
@@ -1,3 +1,5 @@
+# This is the code to preprocess the data
+
 import cv2 as cv
 import numpy as np
 import os
@@ -7,6 +9,7 @@ mypathsave = "data/IA_test/"
 if not os.path.exists(mypathsave):
     os.mkdir(mypathsave)
 folders = os.listdir(mypath)
+# Search images in all subfolders
 for folder in folders:
 
     onlyfiles2 = os.listdir(mypath+str(folder))
@@ -17,33 +20,26 @@ for folder in folders:
 
     for n in range(0, len(onlyfiles2)):
         
+        # Load the image
         print(mypath+link+"/"+onlyfiles2[n])
         src = cv.imread( mypath+link+"/"+onlyfiles2[n] )
         if src is None:
             continue
 
         height, width = src.shape[:2]
-
-        #src = src[90:height-90,170:width-170]
-
-        #src = cv.resize(src,(223,223))
-
-        cv.imwrite(mypathsave+link+"/"+str(n)+"-1.jpg",src)
-
-        #height, width = src.shape[:2]
-
         center = (width/2, height/2)
 
         rotate_matrix = cv.getRotationMatrix2D(center=center, angle=90, scale=1)
 
-        rotated_image = cv.warpAffine(src=src, M=rotate_matrix, dsize=(width, height))
+        # Create and save 4 rotations of the image
 
+        cv.imwrite(mypathsave+link+"/"+str(n)+"-1.jpg",src)
+
+        rotated_image = cv.warpAffine(src=src, M=rotate_matrix, dsize=(width, height))
         cv.imwrite(mypathsave+link+"/"+str(n)+"-2.jpg",rotated_image)
 
         rotated_image = cv.warpAffine(src=rotated_image, M=rotate_matrix, dsize=(width, height))
-
         cv.imwrite(mypathsave+link+"/"+str(n)+"-3.jpg",rotated_image)
 
         rotated_image = cv.warpAffine(src=rotated_image, M=rotate_matrix, dsize=(width, height))
-
         cv.imwrite(mypathsave+link+"/"+str(n)+"-4.jpg",rotated_image)
